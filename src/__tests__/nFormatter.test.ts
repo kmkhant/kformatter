97c9100d1adb5010048071e1322535511c7f9271
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2023-05-05 14:48:04 +0630

    fix: fix test

--- src/__tests__/nFormatter.test.ts
+++ src/__tests__/nFormatter.test.ts
@@ -1,13 +1,13 @@
-import { nFormatter } from '../index';
+import { formatter } from '../index';
 
 test('10e3 k test', () => {
-  expect(nFormatter(1000, 1)).toBe('1k');
+  expect(formatter(1000, 1)).toBe('1k');
 });
 
 test('10e3+digits k test', () => {
-  expect(nFormatter(14113, 1)).toBe('14.1k');
+  expect(formatter(14113, 1)).toBe('14.1k');
 });
 
 test('10e6 M test', () => {
-  expect(nFormatter(1124566, 1)).toBe('1.1M');
+  expect(formatter(1124566, 1)).toBe('1.1M');
 });
