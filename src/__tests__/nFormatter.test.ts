54b7578a6f93bed8a29947e180baa401a4e7c496
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2023-05-05 14:23:00 +0630

    fix:minor fixes

--- src/__tests__/nFormatter.test.ts
+++ src/__tests__/nFormatter.test.ts
@@ -7,3 +7,7 @@ test('10e3 k test', () => {
 test('10e3+digits k test', () => {
   expect(nFormatter(14113, 1)).toBe('14.1k');
 });
+
+test('10e6 M test', () => {
+  expect(nFormatter(1124566, 1)).toBe('1.1M');
+});
